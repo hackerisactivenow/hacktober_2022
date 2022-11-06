c354dc833d80b2d390e73caca1daf900c118fcf3
Wanuja Ranasinghe <63337783+Wanuja97@users.noreply.github.com>
2022-11-06 15:59:16 +0530

    Singleton pattern added

---
+++ SingletonPattern.java
@@ -0,0 +1,28 @@
+public class SingletonPattern {
+    public static void main(String[] args) {
+
+        
+        SingleObject object = SingleObject.getInstance();
+  
+        //show the message
+        object.showMessage();
+     }
+}
+class SingleObject {
+
+    //create an object of SingleObject
+    private static SingleObject instance = new SingleObject();
+ 
+    //make the constructor private so that this class cannot be
+    //instantiated
+    private SingleObject(){}
+ 
+    //Get the only object available
+    public static SingleObject getInstance(){
+       return instance;
+    }
+ 
+    public void showMessage(){
+       System.out.println("Hello World!");
+    }
+ }
